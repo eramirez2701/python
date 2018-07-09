908999dd4d5921e4bdb3ac0cfeae95219acc8e86
Ernest <eramirez2701@me.com>
2018-07-09 17:19:06 -0500

    Fixed hardcoded values in AudioConverter

--- Audio/AudioConverter/AudioConverter.py
+++ Audio/AudioConverter/AudioConverter.py
@@ -14,9 +14,9 @@ def get_filepaths(directory, extension):
 def convert_files(directory, input_audioextension, audio_format, audio_codec):
     filepaths = get_filepaths(directory, input_audioextension)
     for filepath in filepaths:
-        new_filepath = filepath.with_suffix('.mp3')
+        new_filepath = filepath.with_suffix(audio_format)
         print(filepath)
-        input_file = AudioSegment.from_file(filepath, "flac")
+        input_file = AudioSegment.from_file(filepath, input_file)
         input_file.export(new_filepath,
                           format=audio_format,
                           bitrate=audio_codec)
