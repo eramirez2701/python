0e72bcedbe74b50349530ebaafd9a8564a19bf11
Ernest <eramirez2701@me.com>
2018-11-08 20:44:40 -0600

    Set up as Class

--- Audio/AudioConverter/AudioConverter.py
+++ Audio/AudioConverter/AudioConverter.py
@@ -3,34 +3,40 @@ from pydub.utils import mediainfo
 import pathlib
 import gc
 
-
-def get_filepaths(directory, extension):
-    filepaths = []
-    for filepath in pathlib.Path(directory).glob('**/*.' + extension):
-        filepaths.append(filepath)
-    return filepaths
-
-
-def get_tags(file_path):
-    return mediainfo(file_path).get('TAG', {})
+class AudioConverter:
+    def __init__(self, input_path, input_audio_extension, output_audio_extension, codec)
+        self.input_path = input_path
+        self.input_audio_extension = input_audio_extension
+        self.output_audio_extension = output_audio_extension
+        self.codec = codec
 
 
-def convert_files(directory, input_audioextension, audio_format, audio_codec):
-    filepaths = get_filepaths(directory, input_audioextension)
-    for fp in filepaths:
-        try:
-          output_extension = '.' + audio_format
-          new_filepath = fp.with_suffix(output_extension)
-          print('Converting ' + str(fp) + ' to ' + audio_format)
-          input_file = AudioSegment.from_file(fp, input_audioextension)
-          input_file.export(new_filepath,
-                            format=audio_format,
-                            bitrate=audio_codec,
-                            tags=get_tags(str(fp)))
-          print("Exported file: " + str(new_filepath))
-          del input_file
-          gc.collect()
-        except FileNotFoundError:
-            continue
-        except MemoryError:
-            continue
+    def get_filepaths(self):
+        filepaths = []
+        for filepath in pathlib.Path(self.input_path).glob('**/*.' + self.input_audio_extension):
+            filepaths.append(filepath)
+        return filepaths
+
+
+    def get_tags(self, filepath):
+        return mediainfo(filepath).get('TAG', {})
+
+
+    def convert_files(self):
+        filepaths = self.get_filepaths()
+        for fp in filepaths:
+            try:
+                output_extension = '.' + self.output_audio_extension
+                new_filepath = fp.with_suffix(output_extension)
+                print('Converting ' + str(fp) + ' to ' + self.codec)
+                input_file = AudioSegment.from_file(fp, self.input_audio_extension)
+                input_file.export(new_filepath,
+                                  format=self.output_audio_extension,
+                                  bitrate=self.codec,
+                                  tags=self.get_tags(str(fp)))
+                print("Exported file: " + str(new_filepath))
+                del input_file
+                gc.collect()
+            except FileNotFoundError:
+            except MemoryError:
+                continue
